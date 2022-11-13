16af0c70e49e71243bc61233d4700ba487e31a83
ChivoLennon <chivolennon@gmail.com>
2022-11-13 18:55:00 -0300

    Levantamiento de Base de datos v1.0

--- app/src/main/java/com/example/ezbro/IngresoPag.java
+++ app/src/main/java/com/example/ezbro/IngresoPag.java
@@ -9,45 +9,56 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class IngresoPag extends AppCompatActivity {
 
-    EditText usuario, password;
-    Button btnlogin;
+    EditText usuario, contra, contraDos;
+    Button login ;
     DB DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ingreso_pag);
+        setContentView(R.layout.activity_ingresopag);
 
-        usuario = (EditText) findViewById(R.id.txtUsuario1);
-        password = (EditText) findViewById(R.id.txtContra1);
-        btnlogin = (Button) findViewById(R.id.btnLogin1);
+        usuario = (EditText) findViewById(R.id.txtUsuario);
+        contra = (EditText) findViewById(R.id.txtContra);
+        contraDos = (EditText) findViewById(R.id.txtContraDos);
+        login = (Button) findViewById(R.id.btnLogin1);
+
         DB = new DB(this);
 
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String user = usuario.getText().toString();
-                String pass = usuario.getText().toString();
+                String pass = contra.getText().toString();
+                String repass = contraDos.getText().toString();
 
-                if(user.equals("")||pass.equals(""))
-                    Toast.makeText(IngresoPag.this, "El yia kie pero komo", Toast.LENGTH_SHORT).show();
+                if(user.equals("")||pass.equals("")||repass.equals(""))
+                    Toast.makeText(IngresoPag.this, "Ingrese datos", Toast.LENGTH_SHORT).show();
                 else{
-                    Boolean checkuserpass = DB.chequeoPass(user, pass);
-                    if(checkuserpass==true){
-                        Toast.makeText(IngresoPag.this, "Registro completo", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(),SegundaCara.class);
-                        startActivity(intent);
-                    }
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.chequeo(user);
+                        if(checkuser==false){
+                            Boolean insert = DB.insertarDatos(user, pass);
+                            if(insert==true) {
+                                Toast.makeText(IngresoPag.this, "Registro completo", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), segundoActivity.class);
+                                startActivity(intent);
+                            }
+                            else
+                            {
+                                Toast.makeText(IngresoPag.this, "Registro fallida", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(IngresoPag.this, "Usuario ya existe", Toast.LENGTH_SHORT).show();
+                        }}
                     else{
-                        Toast.makeText(IngresoPag.this, "Credencial invalida", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(IngresoPag.this, "Contraseña no valida", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
-
-
-
-    }
-}
+}}
