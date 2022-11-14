87edcb16e3120ed1d882e1629ae629ec77b234c2
ChivoLennon <chivolennon@gmail.com>
2022-11-14 00:05:46 -0300

    Revert "Levantamiento de Base de datos v1.0"
    This reverts commit 16bdae26be60e8f2ad675f2e5fcfadefa4b088c9.

--- app/src/main/java/com/example/ezbro/Login.java
+++ app/src/main/java/com/example/ezbro/Login.java
@@ -1,13 +1,23 @@
 package com.example.ezbro;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLDebugHelper;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,9 +34,8 @@ public class Login extends AppCompatActivity
     NotificationManagerCompat notificationManagerCompat;
     Notification notificacion;
 
-    EditText usuario, password;
-    Button btnlogin;
-    Button registro;
+    EditText usuario, contra, contraDos;
+    Button login, registro;
     DB DB;
 
     @Override
@@ -34,22 +43,24 @@ public class Login extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        usuario = (EditText) findViewById(R.id.txtUsuario1);
-        password = (EditText) findViewById(R.id.txtContra1);
-        btnlogin = (Button) findViewById(R.id.btnLogin);
+        usuario = (EditText) findViewById(R.id.txtUsuario);
+        contra = (EditText) findViewById(R.id.txtContra);
+        contraDos = (EditText) findViewById(R.id.txtContraDos);
+
+        login = (Button) findViewById(R.id.btnLogin);
         registro = (Button) findViewById(R.id.btnRegistro);
+
         DB = new DB(this);
 
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String user = usuario.getText().toString();
-                String pass = password.getText().toString();
+                String pass = contra.getText().toString();
+                String repass = contraDos.getText().toString();
 
                 if(user.equals("")||pass.equals("")||repass.equals(""))
                     Toast.makeText(Login.this, "no se admiten los datos", Toast.LENGTH_SHORT).show();
-                if(user.equals("")||pass.equals(""))
-                    Toast.makeText(Login.this, "Porfavor ingresa tus datos", Toast.LENGTH_SHORT).show();
                 else{
                     if(pass.equals(repass)){
                         Boolean checkuser = DB.chequeo(user);
@@ -68,19 +79,13 @@ public class Login extends AppCompatActivity
                         else {
                             Toast.makeText(Login.this, "Usuario ya existe", Toast.LENGTH_SHORT).show();
                         }}
-                    Boolean checkuserpass = DB.chequeouserPass(user, pass);
-                    if(checkuserpass==true){
-                        Toast.makeText(Login.this, "Has ingresado sin problemas", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(),segundoActivity.class);
-                        startActivity(intent);
-                    }
                     else{
-                        Toast.makeText(Login.this, "Credencial invalida", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Login.this, "Contraseña no valida", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
-
         });
+
         registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,39 +93,6 @@ public class Login extends AppCompatActivity
                 startActivity(intent);
             }
         });
-
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationChannel channel = new NotificationChannel("idNotificacion", "Primera notificación", NotificationManager.IMPORTANCE_DEFAULT);
-
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(channel);
-        }
-        NotificationCompat.Builder format = new NotificationCompat.Builder(this, "idNotificacion")
-                .setSmallIcon(android.R.drawable.stat_notify_sync)
-                .setContentTitle("Primera notificacion")
-                .setContentText("¡Este es un mensaje de notificacion de mi app! ");
-
-        notificacion = format.build();
-        notificationManagerCompat = NotificationManagerCompat.from(this);
-
-        tercerDialogo();
-        segundoDialogo();
-        mostrarDialogo();
-    }
-
-    private void mostrarDialogo()
-    {
-        new AlertDialog.Builder(this)
-                .setTitle("Hola esto es un anuncio!")
-                .setMessage("Esta app esta hecha con la intencion de informar datos y lore de Escape From Tarkov. ¡Disfrutalo!")
-                .setPositiveButton("Esta bien", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogo, int i) {
-                        dialogo.dismiss();
-                    }
-                })
-                .create().show();
 
         juan = (CheckBox)findViewById(R.id.checkNotificacion);
         juan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
@@ -199,39 +171,5 @@ public class Login extends AppCompatActivity
     }
 
 
-    private void segundoDialogo()
-    {
-        new AlertDialog.Builder(this)
-                .setTitle("")
-                .setMessage("Recuerda que esto es un proyecto, no consideres que esté terminado, porque no lo esta jaja´nt")
-                .setPositiveButton("Comprendo", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogo, int i) {
-                        dialogo.dismiss();
-                    }
-                })
-                .create().show();
-    }
-    private void tercerDialogo()
-    {
-        new AlertDialog.Builder(this)
-                .setTitle("")
-                .setMessage("Tambien te recuerdo que soy Simon Sanchez, mas conocido como Chivo, ¡SALUDOS!")
-                .setPositiveButton("Rico", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogo, int i) {
-                        dialogo.dismiss();
-                    }
-                })
-                .create().show();
-    }
-
-
 
 }
-
-
-
-
-
-
